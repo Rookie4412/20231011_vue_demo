163c2c6db79568ee666d557cfeb80fdacfbe27cc
Rookie4412 <4412hank@gmail.com>
2023-12-25 21:40:31 +0800

    threeJs

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,34 @@
-<script setup>
-import HomeView from "./views/HomeView.vue"
-import Header from "../src/components/Header.vue"
-import test from "../src/components/test.vue"
+<script>
+// 前置環境 環境安裝 In json
+// "scripts": {
+//   "serve": "vue-cli-service serve",
+//   "build": "vue-cli-service build",
+//   "lint": "vue-cli-service lint"
+// }放入這個，跑
+// npm run serve 這個
+// 不行再跑 npm install @vue/cli 之後再執行
 
+// 匯入Three
+import ThreeSphere from "./components/ThreeSphere.vue";
+// import HomeView from "./views/HomeView.vue";
+
+export default {
+    name: "App",
+    components: {
+    ThreeSphere,
+    // HomeView
+},
+};
 </script>
 
 <template>
-  <div class="topHeader">
-    <Header />
-  </div>
-  <div class="all">
-    <RouterView />
-  </div>
-  <!-- <HomeView /> -->
-</template>
+    <div id="app">
+            <ThreeSphere />
+        <!-- <HomeView /> -->
+    </div>
+</template> 
 
-<style scoped>
-.headershow {
-  width: 100vw;
-  height: 15vh;
-}
-.all{
-  text-align: center;
-}
-.contenshow {
-  width: 100vw;
-  height: 60vh;
-  text-align: center;
-}
+<style>
+/* Add any global styles if needed */
 </style>
+
